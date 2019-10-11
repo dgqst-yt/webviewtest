c777c6545248a837ad15784a73a7fbd76bb6d7a5
yuuichirou_tanaka <yuuichirou_tanaka@digiq.co.jp>
2019-10-11 09:14:46 +0900

    test

---
+++ app/src/main/java/jp/test/webapp/WebViewActivity.java
@@ -0,0 +1,22 @@
+package jp.test.webapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webviewmain);
+
+
+
+        mWebView = (WebView)findViewById(R.id.webView1);
+        mWebView.loadUrl("https://www.ena.travel/sp/");
+
+    }
+}
